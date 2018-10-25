25ee7c7969efe5bdfd0971fc338617b1916a5687
Feleky Mirkó <feleky@gmail.com>
2018-10-25 14:04:09 +0200

    37 selected events to open

--- src/features/events/EventList/EventList.jsx
+++ src/features/events/EventList/EventList.jsx
@@ -3,13 +3,18 @@ import EventListItem from "./EventListItem.jsx";
 
 class EventList extends Component {
   render() {
-    const { events } = this.props;
+    const { events, onEventEdit } = this.props;
     return (
       <div>
         <h1>Event List</h1>
-        {events && events.map(event => (
-          <EventListItem key={event.id} event={event} />
-        ))}
+        {events &&
+          events.map(event => (
+            <EventListItem
+              key={event.id}
+              event={event}
+              onEventEdit={onEventEdit}
+            />
+          ))}
       </div>
     );
   }
