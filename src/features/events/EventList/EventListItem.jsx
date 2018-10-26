c1e41a5fdd148f27d33063558bcd72155e3f9d50
Feleky Mirkó <feleky@gmail.com>
2018-10-26 13:21:10 +0200

    add static detail page

--- src/features/events/EventList/EventListItem.jsx
+++ src/features/events/EventList/EventListItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import EventListAttendee from "./EventListAttendee";
 
 class EventListItem extends Component {
@@ -43,9 +44,9 @@ class EventListItem extends Component {
             floated="right"
             content="Delete"
           />
-          <Button
-            onClick={onEventOpen(event)}
-            as="a"
+          <Button            
+            as= {Link}
+            to={`/event/${event.id}`}
             color="teal"
             floated="right"
             content="View"
